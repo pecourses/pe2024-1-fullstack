5649bbccf956a1364f312e081de047cdfd6ed157
Viktoriia <freshitpte@gmail.com>
2024-10-12 19:43:17 +0300

    Add seed

---
+++ seeders/20241012161418-add-users.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          nickname: 'test',
+          email: 'mail@mail',
+          tel: '+380123456789',
+          passw_hash: '1234',
+          birthday: '2000-05-06',
+          gender: 'male',
+          created_at: new Date().toISOString(),
+          updated_at: new Date().toISOString(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('users', null, {});
+  },
+};
