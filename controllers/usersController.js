77ea7b2f48fe2bdae9539e71d160db7d27c5a386
Viktoriia <freshitpte@gmail.com>
2024-10-13 17:15:36 +0300

    Add user controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,32 @@
-module.exports.createUser = async (req, res, next) => {};
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const { User } = require('./../models');
+
+module.exports.createUser = async (req, res, next) => {
+  const { body } = req;
+
+  try {
+    const SALT_RAUNDS = 10;
+    body.passwHash = await bcrypt.hash(body.passwHash, SALT_RAUNDS);
+
+    const createdUser = await User.create(body);
+
+    // const prepatedUser = { ...createdUser.get() };
+    // delete prepatedUser.passwHash;
+    // delete prepatedUser.createdAt;
+    // delete prepatedUser.updatedAt;
+
+    const prepatedUser = _.omit(createdUser.get(), [
+      'passwHash',
+      'createdAt',
+      'updatedAt',
+    ]);
+
+    res.status(201).send(prepatedUser);
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports.getUsers = async (req, res, next) => {
   res.status(501).send('GET /api/users Not Implemented');
