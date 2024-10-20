3e7051a400ab75b35ee73959ca1280d5d5fc7fac
Viktoriia <freshitpte@gmail.com>
2024-10-20 16:17:06 +0300

    Add task migration

---
+++ migrations/20241020122056-create-task.js
@@ -0,0 +1,57 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      body: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      deadline: {
+        type: Sequelize.DATEONLY,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+    await queryInterface.addConstraint('tasks', {
+      fields: ['body'],
+      type: 'check',
+      where: {
+        body: {
+          [Sequelize.Op.ne]: '',
+        },
+      },
+    });
+    await queryInterface.addConstraint('tasks', {
+      fields: ['deadline'],
+      type: 'check',
+      where: {
+        deadline: {
+          [Sequelize.Op.gte]: Sequelize.literal('CURRENT_DATE'),
+        },
+      },
+    });
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('tasks');
+  },
+};
