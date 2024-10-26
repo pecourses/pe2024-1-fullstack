14194592ab05389e76c0e1de91b6772780f5f238
Viktoriia <freshitpte@gmail.com>
2024-10-26 14:00:25 +0300

    Add seeders

--- seeders/20241012161418-add-users.js
+++ seeders/20241012161418-add-users.js
@@ -1,5 +1,6 @@
 'use strict';
 const bcrypt = require('bcrypt');
+const { SALT_ROUNDS } = require('../constants');
 
 // шифрування
 // 1234 -> ljfdsklfjsdklf4335245пв2а4ипа52піав
@@ -8,9 +9,6 @@ const bcrypt = require('bcrypt');
 // хешування
 // 1234 -> ljfdsklfjsdklf4335245
 
-// TODO move to constants.js
-const SALT_ROUNDS = Number(process.env.SALT_ROUNDS);
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
@@ -18,14 +16,124 @@ module.exports = {
       'users',
       [
         {
-          nickname: 'test',
-          email: 'mail@mail',
-          tel: '+380123456789',
-          passw_hash: bcrypt.hashSync('1234', SALT_ROUNDS),
-          birthday: '2000-05-06',
+          id: 1,
+          nickname: 'user1',
+          email: 'user1@example.com',
+          tel: '+380501234567',
+          passw_hash: bcrypt.hashSync('password1', SALT_ROUNDS),
+          birthday: '1990-01-15',
           gender: 'male',
-          created_at: new Date().toISOString(),
-          updated_at: new Date().toISOString(),
+          role: 'manager',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 2,
+          nickname: 'user2',
+          email: 'user2@example.com',
+          tel: '0501234568',
+          passw_hash: bcrypt.hashSync('password2', SALT_ROUNDS),
+          birthday: '1985-05-10',
+          gender: 'female',
+          role: 'manager',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 3,
+          nickname: 'user3',
+          email: 'user3@example.com',
+          tel: '+380671234569',
+          passw_hash: bcrypt.hashSync('password3', SALT_ROUNDS),
+          birthday: '1992-12-20',
+          gender: 'male',
+          role: 'executor',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 4,
+          nickname: 'user4',
+          email: 'user4@example.com',
+          tel: '0671234560',
+          passw_hash: bcrypt.hashSync('password4', SALT_ROUNDS),
+          birthday: '1978-09-25',
+          gender: 'female',
+          role: 'executor',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 5,
+          nickname: 'user5',
+          email: 'user5@example.com',
+          tel: '+380931234561',
+          passw_hash: bcrypt.hashSync('password5', SALT_ROUNDS),
+          birthday: '1989-03-18',
+          gender: 'other',
+          role: 'executor',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 6,
+          nickname: 'user6',
+          email: 'user6@example.com',
+          tel: '0931234562',
+          passw_hash: bcrypt.hashSync('password6', SALT_ROUNDS),
+          birthday: '2000-11-30',
+          gender: 'male',
+          role: 'executor',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 7,
+          nickname: 'user7',
+          email: 'user7@example.com',
+          tel: '+380681234563',
+          passw_hash: bcrypt.hashSync('password7', SALT_ROUNDS),
+          birthday: '1995-06-08',
+          gender: 'female',
+          role: 'executor',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 8,
+          nickname: 'user8',
+          email: 'user8@example.com',
+          tel: '0681234564',
+          passw_hash: bcrypt.hashSync('password8', SALT_ROUNDS),
+          birthday: '1980-02-02',
+          gender: 'male',
+          role: 'executor',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 9,
+          nickname: 'user9',
+          email: 'user9@example.com',
+          tel: '+380991234565',
+          passw_hash: bcrypt.hashSync('password9', SALT_ROUNDS),
+          birthday: '1975-08-14',
+          gender: 'female',
+          role: 'executor',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 10,
+          nickname: 'user10',
+          email: 'user10@example.com',
+          tel: '0991234566',
+          passw_hash: bcrypt.hashSync('password10', SALT_ROUNDS),
+          birthday: '1993-07-22',
+          gender: 'other',
+          role: 'executor',
+          created_at: new Date(),
+          updated_at: new Date(),
         },
       ],
       {}
--- seeders/20241020161418-add-tasks.js
+++ seeders/20241020161418-add-tasks.js
@@ -1,28 +1,82 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+
+const { addDays, format } = require('date-fns');
+
+const today = new Date();
+
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert(
       'tasks',
       [
         {
-          body: 'To do hw',
-          deadline: '2025-02-01',
+          body: 'Complete the project documentation',
+          deadline: format(addDays(today, 0), 'yyyy-MM-dd'), // Сьогоднішня дата
+          user_id: 3,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          body: 'Prepare presentation slides for the meeting',
+          deadline: format(addDays(today, 1), 'yyyy-MM-dd'), // Завтра
+          user_id: 4,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          body: 'Review the code for the new feature',
+          deadline: format(addDays(today, 2), 'yyyy-MM-dd'), // Післязавтра
+          user_id: 5,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          body: 'Fix bugs reported by QA team',
+          deadline: format(addDays(today, 0), 'yyyy-MM-dd'),
           user_id: 6,
           created_at: new Date(),
           updated_at: new Date(),
         },
         {
-          body: 'Walk',
-          deadline: '2025-02-01',
-          user_id: 6,
+          body: 'Update the user guide with recent changes',
+          deadline: format(addDays(today, 1), 'yyyy-MM-dd'),
+          user_id: 7,
           created_at: new Date(),
           updated_at: new Date(),
         },
         {
-          body: 'To do hw',
-          deadline: '2025-02-01',
+          body: 'Implement authentication module',
+          deadline: format(addDays(today, 2), 'yyyy-MM-dd'),
           user_id: 8,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          body: 'Organize a team-building event',
+          deadline: format(addDays(today, 0), 'yyyy-MM-dd'),
+          user_id: 9,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          body: 'Create a deployment plan for the new release',
+          deadline: format(addDays(today, 1), 'yyyy-MM-dd'),
+          user_id: 10,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          body: 'Analyze the project requirements',
+          deadline: format(addDays(today, 2), 'yyyy-MM-dd'),
+          user_id: 3,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          body: 'Plan the sprint for next development cycle',
+          deadline: format(addDays(today, 0), 'yyyy-MM-dd'),
+          user_id: 4,
           created_at: new Date(),
           updated_at: new Date(),
         },
@@ -30,6 +84,7 @@ module.exports = {
       {}
     );
   },
+
   async down (queryInterface, Sequelize) {
     await queryInterface.bulkDelete('tasks', null, {});
   },
